3b40d0036dcd12199b29b3dc8df99f760ec0cb0c
Vinícius Pereira <viniciuspsb@gmail.com>
2021-11-01 17:42:12 -0300

    feat(src-index): create translateFile function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,23 @@
-import foobar from './index'
+import { translateFile } from './index'
+import handleFile from './handle-file/handle-file'
 
-describe('Foobar', () => {
-  it('should return foobar', () => {
-    expect(foobar('foo', 'bar')).toBe('foobar')
-  })
+describe('Test Translate File Function', () => {
+  it('should translate CHANGELOG.md file to Portuguese and save with default name', async () => {
+    const result = await translateFile('../../CHANGELOG.md', 'auto', 'pt')
+    handleFile.deleteFile('CHANGELOG_pt.md')
+
+    expect(result).toBe(true)
+  }, 15000)
+
+  it('should translate CHANGELOG.md file to Portuguese and save as CHANGELOG.pt.md', async () => {
+    const result = await translateFile(
+      '../../CHANGELOG.md',
+      'auto',
+      'pt',
+      '../../CHANGELOG.pt.md'
+    )
+    handleFile.deleteFile('CHANGELOG.pt.md')
+
+    expect(result).toBe(true)
+  }, 15000)
 })
